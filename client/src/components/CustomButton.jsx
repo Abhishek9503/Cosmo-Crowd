2e8f44cffc0fa18ac4ac83bd424860951f496846
Abhishek <yadavabhishek9503@gmail.com>
2023-09-22 16:48:41 +0530

    create campaign completed

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const CustomButton = ({btnType, title, handleClick}) => {
+const CustomButton = ({btnType, title, handleClick,styles}) => {
   return (
     <button
     type={btnType}
-    className={`font-epilougue font-semibold  text-[16px] leading-[26px] text-white min-h-[52px] rounded-[10px] $  {styles}`}
+    className={`font-epilougue font-semibold  text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
     onClick={handleClick}
     >
 
